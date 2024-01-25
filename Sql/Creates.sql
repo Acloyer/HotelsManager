52358231d8e84bd0a601c1763e21814ef0fd785a
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-01-25 18:19:29 +0400

    Sql create
    создал sql и добавил данные

---
+++ Sql/Creates.sql
@@ -0,0 +1,9 @@
+create database HotelsDB
+
+use HotelsDB
+
+create table Hotels (
+    [Id] int primary key identity,
+    [Name] nvarchar(100),
+    [Price] money,
+)
---
+++ Sql/Inserts.sql
@@ -0,0 +1,5 @@
+insert into Hotels([Name], [Price])
+values('Hotel1', 10000),
+      ('Hotel2', 20000),
+      ('Hotel3', null),
+      (null, 30000)
