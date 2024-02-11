57418659128294cde31cfa0ce048ae0defb3ddb0
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-02-12 00:47:08 +0400

    Удаление ненужного кода из Home контроллера
    Удалил бесполезный код, и добавил Secret() и Authorize для него.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,42 +1,18 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using HotelsManager.Models;
-using System.Text.Json;
-
 namespace HotelsManager.Controllers;
 
 using System.Diagnostics;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
-using HotelsManager.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using HotelsManager.Models;
+
 
 public class HomeController : Controller
 {
-    public HomeController()
-    {
-        System.Console.WriteLine("HomeController created!");
-    }
-
-    public IActionResult MyMethod(int? id, string? name = "unknown") {
-        return Ok(new {
-            id, name
-        });
-    }
-
-    [HttpGet]
-    public IActionResult Login() {
-        return base.View();
-    }
-    
-    [HttpPost]
-    public IActionResult Login(LoginDto loginDto) {
-        System.Console.WriteLine($"{loginDto.Login} {loginDto.Password}");
-
-        return RedirectToAction("Index");
-    }
-
-
+    // public HomeController()
+    // {
+    // // System.Console.WriteLine("HomeController created!");
+    // }
 
     public IActionResult Index()
     {
@@ -46,10 +22,11 @@ public class HomeController : Controller
 
         return base.View(hotels);
     }
-
-    public IActionResult Privacy()
+    [HttpGet]
+    [Authorize(Policy = "Administrators")]
+    public IActionResult Secret()
     {
-        return base.View();
+        return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
