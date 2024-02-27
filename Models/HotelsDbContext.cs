54e00fd70d58139a22cf114c8eabc25a402ceaa5
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-02-27 22:52:44 +0400

    Deleted some controllers

--- Models/HotelsDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class HotelDbContext : DbContext
-{
-    public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) { }
-
-    public DbSet<Hotel> Hotels { get; set; }
-}
