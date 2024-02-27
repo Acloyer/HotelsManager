6b145e1800bf69eec2599c10d0de4275f123e4e6
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-02-27 22:55:15 +0400

    Update IdentityController.cs

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,27 +1,31 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text.RegularExpressions;
 using System.Security.Claims;
 using System.Text;
+using System.Data;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using HotelsManager.Dtos;
+using HotelsManager.Models.Repository;
+using HotelsManager.Models.Users;
+/*using HotelsManager.Models.ProfileViewModel;*/
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Dapper;
 
 namespace HotelsManager.Controllers
 {
     [Route("[controller]/[action]")]
     public class IdentityController : Controller
     {
-        [HttpGet("/api/[controller]/Claims")]
-        [Authorize]
-        public string GetClaims() {
-            var sb = new StringBuilder(100);
-
-            foreach (var claim in base.User.Claims)
-            {
-                sb.Append($"{claim.Type}: {claim.Value}\n");
-            }
-
-            return sb.ToString();
+        IUserRepository repo;
+        string Password = string.Empty;
+        public IdentityController(IUserRepository r)
+        {
+            repo = r;
         }
 
         [HttpGet]
@@ -40,31 +44,167 @@ namespace HotelsManager.Controllers
             return base.View();
         }
 
+        [HttpGet]
+        [Authorize]
+        public IActionResult Home(string? returnUrl)
+        {
+            base.ViewData["returnUrl"] = returnUrl;
+
+            return base.View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Registration(string? returnUrl) {
+            if (!User.Identity.IsAuthenticated)
+            {
+                base.ViewData["returnUrl"] = returnUrl;
+
+                return base.View();
+            }
+            else
+                return base.RedirectToAction(controllerName: "Home", actionName: "Index");
+        }
+
         [HttpPost]
         [AllowAnonymous]
         public IActionResult Login([FromForm] LoginDto loginDto) {
-            var roleName = loginDto.Login.Contains("admin")
-                ? "admin"
-                : "user";
+            if (!User.Identity.IsAuthenticated)
+            {
+                UserRepository dataAccess = new UserRepository(@"Data Source=LAPTOP-8U7UGFTE;Initial Catalog=HotelsDB;
+									    Integrated Security=SSPI;TrustServerCertificate=True;");
+                List<HotelsManager.Models.Users.User> users = dataAccess.GetUsers();
+                foreach(var user in users)
+                {
+                    if(loginDto.Login == user.Name)
+                    {
+                        if(loginDto.Password == user.Password)
+                        {
+                            var roleName = loginDto.Login.Contains("ADM_")
+                            ? "admin"
+                            : "user";
 
-            var claims = new List<Claim> {
-                new(ClaimTypes.Name, loginDto.Login),
-                new(ClaimTypes.Role, roleName),
-                new("creation_date_utc", DateTime.UtcNow.ToString()),
-            };
+                            var claims = new List<Claim> {
+                                new(ClaimTypes.Name, loginDto.Login),
+                                new("Password", loginDto.Password),
+                                new(ClaimTypes.Role, roleName),
+                                new("creation_date_utc", DateTime.UtcNow.ToString()),
+                            };
 
-            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-            base.HttpContext.SignInAsync(
-                scheme: CookieAuthenticationDefaults.AuthenticationScheme,
-                principal: new ClaimsPrincipal(claimsIdentity)
-            );
+                            base.HttpContext.SignInAsync(
+                            scheme: CookieAuthenticationDefaults.AuthenticationScheme,
+                            principal: new ClaimsPrincipal(claimsIdentity)
+                        );
 
-            if(string.IsNullOrWhiteSpace(loginDto.ReturnUrl)) 
+                        if (string.IsNullOrWhiteSpace(loginDto.ReturnUrl))
+                            return base.RedirectToAction(controllerName: "Home", actionName: "Index");
+                        }
+                    }
+                }
+                return View();
+            }
+            else
                 return base.RedirectToAction(controllerName: "Home", actionName: "Index");
-                
-            return base.RedirectPermanent(loginDto.ReturnUrl);
+
         }
+        [HttpGet]
+        [Authorize]
+        public IActionResult Profile()
+        {
+            var password = repo.GetPassword(repo.GetId(User.Identity.Name).Id).Password;
+            var orders = repo.GetOrdersCount(User.Identity.Name);
+            ProfileViewModel data = new ProfileViewModel(User.Identity.Name, password, orders);
+            return View(data);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Registration([FromForm] LoginDto loginDto) {
+            if (!User.Identity.IsAuthenticated)
+            {
+                UserRepository dataAccess = new UserRepository(@"Data Source=LAPTOP-8U7UGFTE;Initial Catalog=HotelsDB;
+									Integrated Security=SSPI;TrustServerCertificate=True;");
+                List<HotelsManager.Models.Users.User> users = dataAccess.GetUsers();
+                var lastId = 1;
+                if (users != null)
+                {
+                    if (users.Count > 0)
+                    {
+                        lastId = users[users.Count - 1].Id;
+                    }
+                }
+                var canRegister = false;
+                string login = loginDto.Login;
+                string password = loginDto.Password;
+
+                /*if (Regex.IsMatch(login, @"^[a-zA-Z0-9_]{4,19}$") && Regex.IsMatch(password, @"^[a-zA-Z0-9_]{6,19}$"))
+                {*/
+                foreach (var user in users)
+                {
+                    if (user.Name != loginDto.Login)
+                    {
+                        canRegister = true;
+                    }
+                    else
+                    {
+                        canRegister = false;
+                        Console.WriteLine(user.Name + " is exists");
+                        break;
+                        /*return View();*/
+                    }
+                }
+
+                if (canRegister)
+                {
+                    HotelsManager.Models.Users.User user1 = new HotelsManager.Models.Users.User(lastId + 1, loginDto.Login, loginDto.Password);
+                    try
+                    {
+                        Console.WriteLine(loginDto.Login);
+                        repo.Create(user1);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Выводите информацию об ошибке или логируйте ее
+                        Console.WriteLine("An error occurred: " + ex.Message);
+                    }
+                    Console.WriteLine(login);
+                    var roleName = login.Contains("ADM_")
+                            ? "admin"
+                            : "user";
+
+                    var claims = new List<Claim> {
+                            new(ClaimTypes.Name, loginDto.Login),
+                            new("Password", loginDto.Password),
+                            new(ClaimTypes.Role, roleName),
+                            new("creation_date_utc", DateTime.UtcNow.ToString()),
+                        };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    base.HttpContext.SignInAsync(
+                        scheme: CookieAuthenticationDefaults.AuthenticationScheme,
+                        principal: new ClaimsPrincipal(claimsIdentity)
+                    );
+
+                    if (string.IsNullOrWhiteSpace(loginDto.ReturnUrl))
+                        return base.RedirectToAction(controllerName: "Home", actionName: "Index");
+
+                    return base.RedirectPermanent(loginDto.ReturnUrl);
+                }
+                /*}
+                else
+                {
+                    Console.WriteLine("Неправильный логин или пароль!");
+                    return View(); // нужно сделать чтобы текст в html равнялся = "неправильный логин или пароль"
+                }*/
+                return View();
+            }
+            else
+                return base.RedirectToAction(controllerName: "Home", actionName: "Index");
+        }
+
 
         [HttpGet]
         [AllowAnonymous]
