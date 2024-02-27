f88c8f4b52b50d124815a412e607dcee0a324930
Acloyer <93879175+Acloyer@users.noreply.github.com>
2024-02-28 00:13:33 +0400

    добавление конструктора в login dto

--- Dtos/LoginDto.cs
+++ Dtos/LoginDto.cs
@@ -6,5 +6,10 @@ namespace HotelsManager.Dtos
         public string? ReturnUrl { get; set; }
         public string? Login { get; set; }
         public string? Password { get; set; }
+        public LoginDto(string _rUrl, string _login, string _password) { 
+            ReturnUrl = _rUrl;
+            Login = _login;
+            Password = _password;
+        }
     }
 }
